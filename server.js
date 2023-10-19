d928b0035bafba41f738d28043d4d20287adedb4
kmarv <marvkaay@gmail.com>
2023-10-19 15:53:28 +0300

    initial author routes

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+const bookRouter = require('./routes/')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}))
 
 // setting up database connection
 const mongoose = require('mongoose')
@@ -24,5 +28,6 @@ db.once('open', () => console.log('Connected to MongoDB'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 app.listen(process.env.PORT || 3000)
